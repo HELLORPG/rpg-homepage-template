f01db5877faf943582213aacb5c369a10bd9d77e
HELLORPG <hellorpg2017@gmail.com>
2022-08-28 23:48:46 +0800

    :tada: 增加了侧边栏和导航栏的配置文件。

---
+++ docs/.vuepress/navbar/en.ts
@@ -0,0 +1,24 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const en = navbar([
+    {
+        text: "About Me",
+        link: "/#about-me",
+    },
+    {
+        text: "Educations",
+        link: "/#educations"
+    },
+    {
+        text: "Internships",
+        link: "/#internships"
+    },
+    {
+        text: "Contact",
+        link: "/#contact"
+    },
+    {
+        text: "Friends",
+        link: "/#friends"
+    },
+]);
---
+++ docs/.vuepress/navbar/zh.ts
@@ -0,0 +1,24 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const zh = navbar([
+  {
+    text: "关于我",
+    link: "/zh/#关于我"
+  },
+  {
+    text: "教育经历",
+    link: "/zh/#教育经历"
+  },
+  {
+    text: "实习经历",
+    link: "/zh/#实习经历"
+  },
+  {
+    text: "联系我",
+    link: "/zh/#联系我"
+  },
+  {
+    text: "友情链接",
+    link: "/zh/#友情链接"
+  },
+]);
