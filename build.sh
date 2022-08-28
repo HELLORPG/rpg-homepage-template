b181e1c3c38d6c0c25ab16f1bafcef03ebee8509
HELLORPG <hellorpg2017@gmail.com>
2022-08-28 13:48:46 +0800

    :tada: 增加了打包脚本。

---
+++ build.sh
@@ -0,0 +1,20 @@
+npm run docs:build
+
+rm -rf ./docs/.vuepress/dist/intro.html
+rm -rf ./docs/.vuepress/dist/slide.html
+rm -rf ./docs/.vuepress/dist/home.html
+
+rm -rf ./docs/.vuepress/dist/timeline/
+rm -rf ./docs/.vuepress/dist/category/
+rm -rf ./docs/.vuepress/dist/star/
+rm -rf ./docs/.vuepress/dist/slide/
+rm -rf ./docs/.vuepress/dist/encrypted/
+rm -rf ./docs/.vuepress/dist/article/
+rm -rf ./docs/.vuepress/dist/tag/
+rm -rf ./docs/.vuepress/dist/zh/timeline/
+rm -rf ./docs/.vuepress/dist/zh/category/
+rm -rf ./docs/.vuepress/dist/zh/star/
+rm -rf ./docs/.vuepress/dist/zh/slide/
+rm -rf ./docs/.vuepress/dist/zh/encrypted/
+rm -rf ./docs/.vuepress/dist/zh/article/
+rm -rf ./docs/.vuepress/dist/zh/tag/
